e6e0a73cf9bb201df19371ca7b1615da352600d1
iBrRus <v.i.kolesnikov@hotmail.com>
2020-11-19 23:20:32 +0300

    Update 41.46

--- media/lua/shared/TrailersSpawnList.lua
+++ media/lua/shared/TrailersSpawnList.lua
@@ -2,24 +2,24 @@ if VehicleZoneDistribution then -- check if the table exists for backwards compa
 
 -- Parking Stall, common parking stall with random cars, the most used one (shop parking lots, houses etc.)
 VehicleZoneDistribution.parkingstall.vehicles["Base.FirstTrailer"] = {index = -1, spawnChance = 5};
-VehicleZoneDistribution.parkingstall.vehicles["Base.SecondTrailer"] = {index = -1, spawnChance = 5};
+VehicleZoneDistribution.parkingstall.vehicles["Base.SecondTrailer"] = {index = -1, spawnChance = 4};
 VehicleZoneDistribution.parkingstall.vehicles["Base.HomeTrailer"] = {index = -1, spawnChance = 2};
 VehicleZoneDistribution.parkingstall.vehicles["Base.KbacTrailer"] = {index = -1, spawnChance = 1};
 
 
 -- Trailer Parks, have a chance to spawn burnt cars, some on top of each others, it's like a pile of junk cars
 VehicleZoneDistribution.trailerpark.vehicles["Base.FirstTrailer"] = {index = -1, spawnChance = 5};
-VehicleZoneDistribution.trailerpark.vehicles["Base.SecondTrailer"] = {index = -1, spawnChance = 5};
+VehicleZoneDistribution.trailerpark.vehicles["Base.SecondTrailer"] = {index = -1, spawnChance = 4};
 VehicleZoneDistribution.trailerpark.vehicles["Base.HomeTrailer"] = {index = -1, spawnChance = 20};
 
 -- bad vehicles, moslty used in poor area, sometimes around pub etc.
 VehicleZoneDistribution.bad.vehicles["Base.FirstTrailer"] = {index = -1, spawnChance = 5};
-VehicleZoneDistribution.bad.vehicles["Base.SecondTrailer"] = {index = -1, spawnChance = 5};
+VehicleZoneDistribution.bad.vehicles["Base.SecondTrailer"] = {index = -1, spawnChance = 4};
 VehicleZoneDistribution.bad.vehicles["Base.HomeTrailer"] = {index = -1, spawnChance = 5};
 
 -- medium vehicles, used in some of the good looking area, or in suburbs
 VehicleZoneDistribution.medium.vehicles["Base.FirstTrailer"] = {index = -1, spawnChance = 5};
-VehicleZoneDistribution.medium.vehicles["Base.SecondTrailer"] = {index = -1, spawnChance = 5};
+VehicleZoneDistribution.medium.vehicles["Base.SecondTrailer"] = {index = -1, spawnChance = 4};
 VehicleZoneDistribution.medium.vehicles["Base.HomeTrailer"] = {index = -1, spawnChance = 5};
 
 -- good vehicles, used in good looking area, they're meant to spawn only good cars, so they're on every good looking house.
@@ -29,7 +29,7 @@ VehicleZoneDistribution.good.vehicles["Base.HomeTrailer"] = {index = -1, spawnCh
 
 -- junkyard, spawn damaged & burnt vehicles, less chance of finding keys but more cars.
 -- also used for the random car crash.
-VehicleZoneDistribution.junkyard.vehicles["Base.FirstTrailer"] = {index = -1, spawnChance = 10};
+VehicleZoneDistribution.junkyard.vehicles["Base.FirstTrailer"] = {index = -1, spawnChance = 8};
 VehicleZoneDistribution.junkyard.vehicles["Base.SecondTrailer"] = {index = -1, spawnChance = 5};
 VehicleZoneDistribution.junkyard.vehicles["Base.HomeTrailer"] = {index = -1, spawnChance = 5};
 VehicleZoneDistribution.junkyard.vehicles["Base.KbacTrailer"] = {index = -1, spawnChance = 1};
@@ -43,7 +43,7 @@ VehicleZoneDistribution.trafficjamw.vehicles["Base.KbacTrailer"] = {index = -1, 
 
 VehicleZoneDistribution.farm = VehicleZoneDistribution.farm or {}
 VehicleZoneDistribution.farm.vehicles = VehicleZoneDistribution.farm.vehicles or {}
-VehicleZoneDistribution.farm.vehicles["Base.FirstTrailer"] = {index = -1, spawnChance = 10};
+VehicleZoneDistribution.farm.vehicles["Base.FirstTrailer"] = {index = -1, spawnChance = 8};
 VehicleZoneDistribution.farm.vehicles["Base.SecondTrailer"] = {index = -1, spawnChance = 15};
 VehicleZoneDistribution.farm.vehicles["Base.HomeTrailer"] = {index = -1, spawnChance = 15};
 
